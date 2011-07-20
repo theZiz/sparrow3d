20bd22770c7d6900714208063caf03d19a74b314
Ziz <zizsdl@googlemail.com>
2011-07-20 14:03:06 +0200

    it starts...

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 #==PC==
-#CPP = gcc -g -march=native -DX86CPU
-#SDL = `sdl-config --cflags`
+CPP = gcc -g -march=native -DX86CPU
+SDL = `sdl-config --cflags`
 #==Consoles==
 #==GP2X/WIZ==
 #ORIGINALFW = -static -lfreetype -lpng -lpthread -lz -ljpeg -lm -s
-CPP = /opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/arm-open2x-linux-gcc -DMOBILE_DEVICE -DARMCPU -DGP2X
-SDL = `/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/sdl-config --cflags`
-INCLUDE = -I/opt/open2x/gcc-4.1.1-glibc-2.3.6/include
-LIB = -L/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib -Wl,-rpath=/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib
+#CPP = /opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/arm-open2x-linux-gcc -DMOBILE_DEVICE -DARMCPU -DGP2X
+#SDL = `/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/sdl-config --cflags`
+#INCLUDE = -I/opt/open2x/gcc-4.1.1-glibc-2.3.6/include
+#LIB = -L/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib -Wl,-rpath=/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib
 #==Caanoo==
 #CPP = /opt/caanoo/gcc-4.2.4-glibc-2.7-eabi/bin/arm-gph-linux-gnueabi-gcc -DMOBILE_DEVICE -DARMCPU -DCAANOO
 #SDL = -I/opt/caanoo/gcc-4.2.4-glibc-2.7-eabi/arm-gph-linux-gnueabi/sys-root/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
@@ -24,7 +24,13 @@ LIB = -L/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib -Wl,-rpath=/opt/open2x/gcc-4.1.1-g
 #INCLUDE = -I/opt/pandora/arm-2011.03/usr/include
 #LIB = -L/opt/pandora/arm-2011.03/usr/lib -Wl,-rpath=/opt/pandora/arm-2011.03/usr/lib -lpnd
 
-all: testengine
+all: sparrow3d testsparrow
+
+testsparrow: testsparrow.c sparrow3d
+	$(CPP) -O3 testsparrow.c $(SDL) $(INCLUDE) -L. $(LIB) -lSDL_ttf -lSDL_image -lSDL -lm -lsparrow3d $(ORIGINALFW) -o testsparrow
+
+sparrow3d: sparrowCore.o sparrowMath.o
+	$(CPP) -shared -Wl,-soname,libsparrow3d.so -o libsparrow3d.so sparrowCore.o sparrowMath.o $(SDL) $(INCLUDE) $(LIB) -lSDL_ttf -lSDL_image -lSDL -lm $(ORIGINALFW)
 
 testengine: testengine.c meshloader.o 3dengine.o graphicstuff.o graphicstuff-asm.o
 	$(CPP) -O3 testengine.c 3dengine.o graphicstuff.o meshloader.o graphicstuff-asm.o $(SDL) $(INCLUDE) $(LIB) -lSDL_ttf -lSDL_image -lSDL -lm $(ORIGINALFW) -o testengine
@@ -41,6 +47,13 @@ graphicstuff.o: graphicstuff.c graphicstuff.h graphicstuff.o
 graphicstuff-asm.o: graphicstuff-asm.c graphicstuff.h
 	$(CPP) -O2 -c graphicstuff-asm.c $(SDL) $(INCLUDE)
 
+sparrowCore.o: sparrowCore.c sparrowCore.h sparrowCore.o
+	$(CPP) -O3 -fPIC -c sparrowCore.c $(SDL) $(INCLUDE)
+
+sparrowMath.o: sparrowMath.c sparrowMath.h sparrowMath.o
+	$(CPP) -O3 -fPIC -c sparrowMath.c $(SDL) $(INCLUDE)
+
 clean:
 	rm *.o
-	rm testengine	
+	rm libsparrow3d.so
+	rm testsparrow
---
+++ sparrowDefines.h
@@ -0,0 +1,149 @@
+/*
+ The contents of this file are subject to the Mozilla Public License        
+ Version 1.1 (the "License"); you may not use this file except in           
+ compliance with the License. You may obtain a copy of the License at       
+ http://www.mozilla.org/MPL/                                                
+                                                                            
+ Software distributed under the License is distributed on an "AS IS"        
+ basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the    
+ License for the specific language governing rights and limitations         
+ under the License.                                                         
+                                                                            
+ Alternatively, the contents of this file may be used under the terms       
+ of the GNU Lesser General Public license (the  "LGPL License"), in which case the  
+ provisions of LGPL License are applicable instead of those                  
+ above.                                                                     
+                                                                            
+ For feedback and questions about my Files and Projects please mail me,     
+ Alexander Matthes (Ziz) , zizsdl_at_googlemail.com                         
+*/
+#ifndef _SPARROW_DEFINES_H
+#define _SPARROW_DEFINES_H
+
+#ifdef WIN32
+  #include <windows.h>
+  #if BUILDING_DLL
+    # define DLLIMPORT __declspec (dllexport)
+  #else /* Not BUILDING_DLL */
+    # define DLLIMPORT __declspec (dllimport)
+  #endif /* Not BUILDING_DLL */
+  #ifdef __cplusplus
+    #define PREFIX extern "C" DLLIMPORT
+  #else
+    #define PREFIX DLLIMPORT
+  #endif
+#else
+  #ifdef __cplusplus
+    #define PREFIX extern "C"
+  #else
+    #define PREFIX  
+  #endif
+#endif
+
+#define SP_ACCURACY 16
+#define SP_HALF_ACCURACY 8
+#define SP_ACCURACY_FACTOR 65536.0
+#define SP_PI 3217*64
+#define SP_COS_ACCURACY 4
+
+#define SP_JOYSTICK_MIN -16384
+#define SP_JOYSTICK_MAX  16383
+
+#ifdef GP2X
+  #define SP_AXIS_UP 0
+  #define SP_AXIS_LEFTUP 1
+  #define SP_AXIS_LEFT 2
+  #define SP_AXIS_LEFTDOWN 3
+  #define SP_AXIS_DOWN 4
+  #define SP_AXIS_RIGHTDOWN 5
+  #define SP_AXIS_RIGHT 6
+  #define SP_AXIS_RIGHTUP 7
+  #define SP_BUTTON_START 8
+  #define SP_BUTTON_SELECT 9
+  #define SP_BUTTON_L 10
+  #define SP_BUTTON_R 11
+  #define SP_BUTTON_A 12
+  #define SP_BUTTON_B 13
+  #define SP_BUTTON_X 14
+  #define SP_BUTTON_Y 15
+  #define SP_BUTTON_VOLPLUS 16
+  #define SP_BUTTON_VOLMINUS 17
+  #define SP_BUTTON_TIP 18
+  //just for different SP_PRActices
+  #define SP_PRACTICE_OK SP_BUTTON_B
+#elif defined CAANOO
+  #define SP_BUTTON_A 0
+  #define SP_BUTTON_X 1
+  #define SP_BUTTON_B 2
+  #define SP_BUTTON_Y 3
+  #define SP_BUTTON_L 4
+  #define SP_BUTTON_R 5
+  #define SP_BUTTON_START 6
+  #define SP_BUTTON_TIP 7
+  #define SP_BUTTON_SELECT 7
+  #define SP_BUTTON_VOLPLUS 8
+  #define SP_BUTTON_VOLMINUS 9
+  #define SP_PRACTICE_OK SP_BUTTON_B
+#else //PC and DINGOO and PANDORA
+  #define SP_BUTTON_START 8
+  #define SP_BUTTON_SELECT 9
+  #define SP_BUTTON_L 10
+  #define SP_BUTTON_R 11
+  #define SP_BUTTON_A 12
+  #define SP_BUTTON_B 13
+  #define SP_BUTTON_X 14
+  #define SP_BUTTON_Y 15
+  #define SP_BUTTON_VOLPLUS 16
+  #define SP_BUTTON_VOLMINUS 17
+  #define SP_BUTTON_TIP 18
+  #define SP_PRACTICE_OK SP_BUTTON_A
+#endif
+
+#ifdef GP2X
+  #define SP_BUTTON_START_NAME "Start"
+  #define SP_BUTTON_SELECT_NAME "Select"
+  #define SP_BUTTON_A_NAME "A"
+  #define SP_BUTTON_B_NAME "B"
+  #define SP_BUTTON_X_NAME "X"
+  #define SP_BUTTON_Y_NAME "Y"
+  #define SP_PAD_NAME "D-Pad"
+  #define SP_PRACTICE_OK_NAME SP_BUTTON_B_NAME
+#elif defined CAANOO
+  #define SP_BUTTON_START_NAME "Start"
+  #define SP_BUTTON_SELECT_NAME "Select"
+  #define SP_BUTTON_A_NAME "A"
+  #define SP_BUTTON_B_NAME "B"
+  #define SP_BUTTON_X_NAME "X"
+  #define SP_BUTTON_Y_NAME "Y"
+  #define SP_PAD_NAME "Stick"
+  #define SP_PRACTICE_OK_NAME SP_BUTTON_B_NAME
+#elif defined DINGOO
+  #define SP_BUTTON_START_NAME "Start"
+  #define SP_BUTTON_SELECT_NAME "Select"
+  #define SP_BUTTON_A_NAME "A"
+  #define SP_BUTTON_B_NAME "B"
+  #define SP_BUTTON_X_NAME "X"
+  #define SP_BUTTON_Y_NAME "Y"
+  #define SP_PAD_NAME "D-Pad"
+  #define SP_PRACTICE_OK_NAME SP_BUTTON_A_NAME  
+#elif defined PANDORA
+  #define SP_BUTTON_START_NAME "Start"
+  #define SP_BUTTON_SELECT_NAME "Select"
+  #define SP_BUTTON_A_NAME "A"
+  #define SP_BUTTON_B_NAME "B"
+  #define SP_BUTTON_X_NAME "X"
+  #define SP_BUTTON_Y_NAME "Y"
+  #define SP_PAD_NAME "D-Pad"
+  #define SP_PRACTICE_OK_NAME SP_BUTTON_A_NAME  
+#else
+  #define SP_BUTTON_START_NAME "Return"
+  #define SP_BUTTON_SELECT_NAME "Backspace"
+  #define SP_BUTTON_A_NAME "a"
+  #define SP_BUTTON_B_NAME "d"
+  #define SP_BUTTON_X_NAME "s"
+  #define SP_BUTTON_Y_NAME "w"
+  #define SP_PAD_NAME "Arrow Keys"
+  #define SP_PRACTICE_OK_NAME SP_BUTTON_A_NAME  
+#endif
+
+#endif
