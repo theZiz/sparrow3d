fbd2336a41154b8831a45c23dec2bc4835b24f7b
Ziz <zizsdl@googlemail.com>
2013-04-06 16:18:03 +0200

    Removed -DFAST_BUT_UGLY

--- target-files/gcw.mk
+++ target-files/gcw.mk
@@ -1,6 +1,6 @@
 # === Target file for the GCW. Use it with "make TARGET=gcw" ===
 
-CPP = /opt/gcw0-toolchain/usr/bin/mipsel-linux-gcc -DMOBILE_DEVICE -DGCW -DFAST_BUT_UGLY $(GENERAL_TWEAKS) $(SMALL_RESOLUTION_DEVICES)
+CPP = /opt/gcw0-toolchain/usr/bin/mipsel-linux-gcc -DMOBILE_DEVICE -DGCW $(GENERAL_TWEAKS) $(SMALL_RESOLUTION_DEVICES)
 SDL = -I/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
 INCLUDE = -I/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include
 LIB = -L/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib -Wl,-rpath=/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib
