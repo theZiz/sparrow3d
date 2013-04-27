9bc26637aed64e302f8064cd8b5ebab3bde5f472
Ziz <zizsdl@googlemail.com>
2013-04-27 09:51:24 +0200

    Added documentation of sparrowMath

--- sparrowMath.h
+++ sparrowMath.h
@@ -17,7 +17,9 @@
  Alexander Matthes (Ziz) , zizsdl_at_googlemail.com
 */
 
-/* This file is for fast functions for fixed point numbers like
+/* File: sparrowMath
+ * 
+ * This file is for fast functions for fixed point numbers like
  * trigonometric functions, square root, etc. */
 #ifndef _SPARROW_MATH_H
 #define _SPARROW_MATH_H
@@ -25,43 +27,158 @@
 #include "sparrowDefines.h"
 #include <SDL.h>
 
-/* spInitMath initializes the fixed point look up tables for sin, cos, etc. */
+/* Function: spInitMath
+ * 
+ * Initializes the fixed point look up tables for sin, cos, etc. */
 PREFIX void spInitMath( void );
 
-/* spSin calculates the sin of value*/
+/* Function: spSin
+ * 
+ * Calculates the sinus of a fixed point value
+ * 
+ * Parameters:
+ * value - fixed point radian value
+ * 
+ * Returns:
+ * Sint32 - fixed point result
+ * 
+ * See Also:
+ * <spCos>, <spTan>*/
 PREFIX Sint32 spSin( Sint32 value );
 
-/* spCos calculates the cos of value*/
+/* Function: spCos
+ * 
+ * Calculates the cosinus of a fixed point value
+ * 
+ * Parameters:
+ * value - fixed point radian value
+ * 
+ * Returns:
+ * Sint32 - fixed point result
+ * 
+ * See Also:
+ * <spSin>, <spTan>*/
 PREFIX Sint32 spCos( Sint32 value );
 
-/* spTan calculates the cos of value*/
+/* Function: spTan
+ * 
+ * Calculates the tangent of a fixed point value
+ * 
+ * Parameters:
+ * value - fixed point radian value
+ * 
+ * Returns:
+ * Sint32 - fixed point result
+ * 
+ * See Also:
+ * <spSin>, <spCos>*/
 PREFIX Sint32 spTan( Sint32 value );
 
-/* spAcos calculates the acos of value. For values smaller than -1 and bigger
- * than 1, it returns 0. */
-PREFIX Sint32 spAcos(Sint32 value);
-
-/* spAcos calculates the asin of value. For values smaller than -1 and bigger
- * than 1, it returns 0. */
+/* Function: spAsin
+ * 
+ * Calculates the arcus sinus of a fixed point value
+ * 
+ * Parameters:
+ * value - fixed point radian value
+ * 
+ * Returns:
+ * Sint32 - fixed point result. If the value out of the range (not between
+ * -<SP_ONE> and <SP_ONE>) zero is returned. If you want some error handling, do
+ * it yourself.
+ * 
+ * See Also:
+ * <spAcos>*/
 PREFIX Sint32 spAsin(Sint32 value);
 
-/* spSqrt fast square root*/
+/* Function: spAcos
+ * 
+ * Calculates the arcus cosinus of a fixed point value
+ * 
+ * Parameters:
+ * value - fixed point radian value
+ * 
+ * Returns:
+ * Sint32 - fixed point result. If the value out of the range (not between
+ * -<SP_ONE> and <SP_ONE>) zero is returned. If you want some error handling, do
+ * it yourself.
+ * 
+ * See Also:
+ * <spAsin>*/
+PREFIX Sint32 spAcos(Sint32 value);
+
+/* Function: spSqrt
+ * 
+ * Fast square root for fixed point numbers
+ * 
+ * Parameters:
+ * n - the fixed point value to be square rooted
+ * 
+ * Returns:
+ * Sint32 - square root of n*/
 PREFIX Sint32 spSqrt ( Sint32 n );
 
-/* This functions returns the square root of a number between 0 and
- * 1 << SP_SQRT_ACCURACY. If you give a number outside this range
- * IT WILL CRASH! So just use spSqrt to be safe. This function is
- * necessary for a good looking light calculation */
+/* Function: spUnsave_Small_Sqrt
+ * 
+ * This functions returns the square root of a number between 0 and
+ * 1 << <SP_SQRT_ACCURACY>. If you give a number outside this range
+ * IT WILL CRASH! So use spSqrt if you are unsure. The light calculation uses
+ * this function for fast rendering.
+ * 
+ * Parameters:
+ * n - as said a fixed point value between 0 and 1 << <SP_SQRT_ACCURACY>
+ * 
+ * Returns:
+ * Sint32 - the square root of n. Always keep in mind, that the normal fixed
+ * point numbers with shift of <SP_ACCURACY> may not be compatible to this fixed
+ * point numbers with shift of <SP_SQRT_ACCURACY>!*/
 PREFIX Sint32 spUnsave_Small_Sqrt(Sint32 n);
 
-/* spMin return the smallest of 2 numbers */
+/* Function: spMin
+ * 
+ * Returns the smallest of 2 numbers. To get the smallest of three and more
+ * numbers, use a tree like function call like: spMin(spMin(a,b),c) or
+ * spMin(spMin(a,b),spMin(c,d)).
+ * 
+ * Parameters:
+ * a,b - numbers to compare
+ * 
+ * Returns:
+ * Sint32 - the smaller number. If the numbers are even, it returns the bigger
+ * on.
+ * 
+ * See Also:
+ * <spMax>*/
 PREFIX Sint32 spMin( Sint32 a, Sint32 b );
 
-/* spMax return the biggest of 2 numbers */
+/* Function: spMax
+ * 
+ * Returns the biggest of 2 numbers. To get the biggest of three and more
+ * numbers, use a tree like function call like: spMax(spMax(a,b),c) or
+ * spMax(spMax(a,b),spMax(c,d)).
+ * 
+ * Parameters:
+ * a,b - numbers to compare
+ * 
+ * Returns:
+ * Sint32 - the bigger number. If the numbers are even, it returns the smaller
+ * on.
+ * 
+ * See Also:
+ * <spMin>*/
 PREFIX Sint32 spMax( Sint32 a, Sint32 b );
 
-/* Converts a string to a Sint32 fixed point number like atof. Be careful, it
- * is slow, especially on devices without FPU. But perfect for loading stuff. */
+/* Function: spAtof
+ * 
+ * Converts a string to a Sint32 fixed point number like atof. Be careful, it
+ * is slow, especially on devices without FPU. But perfect for loading stuff.
+ * 
+ * Parameters:
+ * buffer - string to convert
+ * 
+ * Returns:
+ * Sint32 - the fixed point value stored in buffer. That means, if buffer is
+ * "12.3" the fixed point conversion of 12.3 will be saved, which is 806092 and
+ * not 12. ;)*/
 PREFIX Sint32 spAtof( char* buffer );
 
 #endif
