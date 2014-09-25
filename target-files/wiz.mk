c693e48940e1a5f7320492d0d25006710002eb8f
Babs Seed <babs@Manehattan>
2014-09-25 17:32:35 +0200

    I added an AR for every target - I have no idea, whether this is necessary, but!

--- target-files/wiz.mk
+++ target-files/wiz.mk
@@ -6,3 +6,4 @@ DYNAMIC = -lSDL_mixer -lSDL_ttf -lSDL_image -lSDL -lm
 SDL = `/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/sdl-config --cflags`
 INCLUDE = -I/opt/open2x/gcc-4.1.1-glibc-2.3.6/include
 LIB = -L/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib -Wl,-rpath=/opt/open2x/gcc-4.1.1-glibc-2.3.6/lib
+AR = /opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/arm-open2x-linux-ar
